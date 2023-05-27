5b8acb84bebae9aebb7eca60323ddc3c187aa100
Tarun Mittal <tm6622@g.rit.edu>
2023-05-27 14:03:16 -0400

    added firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBpo0WCfWO5hiI1ug4cmr2SXeIQewUP9r4",
+    authDomain: "crwn-clothing-db-51306.firebaseapp.com",
+    projectId: "crwn-clothing-db-51306",
+    storageBucket: "crwn-clothing-db-51306.appspot.com",
+    messagingSenderId: "1068649775929",
+    appId: "1:1068649775929:web:127a362d1dac6a6ef65ee8"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
