201de13660a7d75356b2376ae1a53c565caff973
Tarun Mittal <tm6622@g.rit.edu>
2023-05-27 14:39:07 -0400

    added createuserdocument method

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -23,15 +23,21 @@ const firebaseConfig = {
 
 
   // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-  provider.setCustomParameters({
+provider.setCustomParameters({
     prompt: "select_account"
-  });
+});
 
-  export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-  export const db = getFirestore();
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+};
