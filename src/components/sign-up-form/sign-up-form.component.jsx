cd9a33e23b81f8923807f2813377efd4969fb684
Tarun Mittal <tm6622@g.rit.edu>
2023-06-02 11:57:05 -0400

    added reset form fields

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -13,6 +13,10 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -28,6 +32,7 @@ const SignUpForm = () => {
                 );
 
             await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
           } catch(error) {
             if(error.code === 'auth/email-already-in-use') {
                 alert('Cannot create user, email already in use');
